701b4f2754db7b22e57a5d41502fb550e5f188e8
Lucas Eduardo <lucased78@hotmail.com>
2022-07-22 16:29:03 -0300

    feat: adding TodosController

---
+++ Controllers/TodosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Models;
+
+namespace TodoAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TodosController : ControllerBase
+  {
+    private readonly TodoContext _context;
+
+    public TodosController (TodoContext context)
+    {
+      _context = context;
+    }
+  }
+}
