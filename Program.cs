a61f6f31fd0651a33f41f91bf3fcddc84071cd6a
Lucas Eduardo <lucased78@hotmail.com>
2022-07-22 16:28:35 -0300

    feat: adding database context to app config

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TodoAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("Todo"));
 
 var app = builder.Build();
 
