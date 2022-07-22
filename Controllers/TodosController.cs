ff94d15e1d1dca916a37d192a94db1dfce2a7feb
Lucas Eduardo <lucased78@hotmail.com>
2022-07-22 17:13:29 -0300

    feat: adding post method for creating todos and get for retrieving them

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -14,5 +14,31 @@ namespace TodoAPI.Controllers
     {
       _context = context;
     }
+
+    [HttpGet]
+    public ActionResult<List<Todo>> Get()
+    {
+      return _context.Todos.ToList();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Todo>> Post(Todo? todo)
+    {
+      if (todo is null)
+      {
+        return BadRequest();
+      }
+
+      if (todo.Id is null)
+      {
+        todo.Id = new Random().Next(0, int.MaxValue);
+      }
+
+      _context.Todos.Add(todo);
+
+      await _context.SaveChangesAsync();
+
+      return Created("", todo);
+    }
   }
 }
